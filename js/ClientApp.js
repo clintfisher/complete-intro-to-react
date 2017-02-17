c8abad96f100944b21f1728ad0ef9a6859ea8df9
clintfisher <clintfisher.is@gmail.com>
2017-02-17 08:33:24 -0800

    converting to jsx

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,20 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
 
   render () {
     return (
-      div(null,
-        MyTitleFactory({title: 'props are the best', color: 'tomato'}),
-        MyTitleFactory({title: 'semicolons are the worst', color: 'peru'}),
-        MyTitleFactory({title: 'jklol its ok if you like semicolons', color: 'mediumaquamarine'}),
-        MyTitleFactory({title: 'im out of ideas', color: 'blanchedalmond'})
-      )
+      <div>
+        <MyTitle title='props are the best' color='rebeccapurple' />
+        <MyTitle title='semicolons are the worst' color='mediumaquamarine' />
+        <MyTitle title='jklol its ok if you like semicolons' color='peru' />
+      </div>
     )
   }
 })
