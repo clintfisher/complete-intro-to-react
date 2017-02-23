9f6123cabe02cb78ce0714ae97c9bfb2992bf6d4
clintfisher <clintfisher.is@gmail.com>
2017-02-23 14:52:17 -0800

    hooking up details via router pretty sweet

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -13,12 +13,21 @@ const App = React.createClass({
     return (
       <BrowserRouter>
         <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
+          <Match
+            exactly pattern='/'
+            component={Landing}
+          />
           <Match
             pattern='/search'
             component={(props) => <Search shows={preload.shows} {...props} />}
-            />
-          <Match pattern='/details/:id' component={Details} />
+          />
+          <Match
+            pattern='/details/:id'
+            component={(props) => {
+              const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
+              return <Details show={shows[0]} {...props} />
+            }}
+          />
         </div>
       </BrowserRouter>
     )
