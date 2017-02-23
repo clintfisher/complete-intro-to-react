207e7aec94b8428464137bad052be34d1166a257
clintfisher <clintfisher.is@gmail.com>
2017-02-23 10:32:22 -0800

    doing some filtering on the response based on the input value

--- js/Search.js
+++ js/Search.js
@@ -5,7 +5,7 @@ import preload from '../public/data.json'
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'this is the default string'
+      searchTerm: ''
     }
   },
   handleSearchTermChange (event) {
@@ -17,15 +17,20 @@ const Search = React.createClass({
     return (
       <div className='search'>
         <header>
-          <h1>{this.state.searchTerm}</h1>
+          <h1>svideo</h1>
           <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
-          {preload.shows.map((show) => {
-            return (
-              <ShowCard key={show.imdbID} {...show} />
-            )
-          })}
+          {preload.shows
+            .filter((show) => {
+              return `${show.title} ${show.description}`
+              .toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            })
+            .map((show) => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+              )
+            })}
         </div>
       </div>
     )
