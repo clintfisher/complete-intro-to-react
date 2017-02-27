6bf4e8e189ccf56cb18a40fdee1f730b04e46672
clintfisher <clintfisher.is@gmail.com>
2017-02-27 13:55:12 -0800

    make an ajax request with axios in componentDidMount to add a rating value to the show from omdbData api

--- js/Details.js
+++ js/Details.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Header from './Header'
 
 const {shape, string} = React.PropTypes
@@ -10,18 +11,37 @@ const Details = React.createClass({
       year: string,
       poster: string,
       trailer: string,
-      description: string
+      description: string,
+      imdbID: string
     })
+  },
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+    .then((response) => this.setState({omdbData: response.data}))
+    .catch((error) => console.error('axios error', error))
   },
   render () {
     const {title, description, year, poster, trailer} = this.props.show
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    } else {
+      rating = <img src='/pubic/img/loading.png' alt='loading' />
+    }
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/public/img/posters/${poster}`} />
+          <p>{description}</p>
         </section>
         <div>
           <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder='0' allowFullScreen />
